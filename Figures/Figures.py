8a9f06b1544db355a88d1310a006fedfae60c5af
Vladislav Jovanov <vladislav.jovanov@ihtm.bg.ac.rs>
2025-08-22 20:12:25 +0200

    submodule update

--- Figures/Figures.py
+++ Figures/Figures.py
@@ -18,7 +18,6 @@ class FigureXY2(Figure):
         self.set_size_inches((11/2.54,8/2.54))
         self._axy=self.add_axes([2/11,2/11,7/11,5/8])
         self._axy.tick_params(labelsize=8)
-        self._axy.tick_params(axis='y',labelcolor='tab:blue')
 
         self._y2=y2
 
@@ -26,6 +25,7 @@ class FigureXY2(Figure):
             #linked right axes  https://matplotlib.org/stable/gallery/subplots_axes_and_figures/two_scales.html#sphx-glr-gallery-subplots-axes-and-figures-two-scales-py
             self._axy2=self._axy.twinx()
             self._axy2.tick_params(labelsize=8)
+            self._axy.tick_params(axis='y',labelcolor='tab:blue')
             self._axy2.tick_params(axis='y',labelcolor='tab:red')
 
         self._init_figure()
@@ -33,11 +33,10 @@ class FigureXY2(Figure):
     def __str__(self):
         return 'v_draw'
 
-    def _draw(self):
-        pass
-
-    def add_draw(self,command):
-        self._draw=command
+    
+    def add_canvas(self,canvas):
+        self.canvas=canvas
+        self.canvasdraw=self.canvas.draw
 
     def _update_x_label(self,xname):
         self._axy.set_xlabel(xname,fontsize=10, position=(0.5,0),labelpad=5)
@@ -51,13 +50,12 @@ class FigureXY2(Figure):
 
     #initlizes the figure
     def _init_figure(self):
-        self._axy.plot([],[],color='tab:blue')
         self._axy.set_xlim(0,1)
         self._axy.set_ylim(0,1)
         if self._y2:
+            self._axy.plot([],[],color='tab:blue')
             self._axy2.plot([],[],color='tab:red')
             self._axy2.set_ylim(0,1)
-        self._draw()
 
     #if x negative i start from 1.02xmin, if x positive I start from 0
     #0.98 is here if I don't want to use 0 as reference
@@ -70,7 +68,7 @@ class FigureXY2(Figure):
 
     #not private
     #plots the data it receives (appending should be done in main if needed)
-    def plot_data(self,x,y,y2=None):
+    def plot_measured_data(self,x,y,y2=None):#x, y, y2 are just numpy arrays
         if len(x)==len(y):
             self._axy.set_xlim(self._find_min(x),self._find_max(x))
             self._axy.set_ylim(self._find_min(y),self._find_max(y))
@@ -83,7 +81,17 @@ class FigureXY2(Figure):
 
             if len(self._axy2.lines)!=0:
                 self._axy2.lines[-1].set_ydata(y2)
-        self._draw()
+        self.canvasdraw()
+
+
+    def plot_loaded_curves(self,datalist=[],masklist=[]):#datalist is list of datadictionaries masklist is a list of checkboxes states
+        if len(datalist)==len(masklist) and len(datalist)!=0:
+            self._axy.set_prop_cycle(None)
+            for idx,item in enumerate(masklist):
+                if item == 'on':
+                    self._axy.plot(datalist[idx]['#data_table'][:,0],datalist[idx]['#data_table'][:,1])
+                elif item == 'off':
+                    self._axy.plot([],[])
 
     #appends and then plots data
     def append_plot_data(self,x,y,y2):
@@ -101,7 +109,7 @@ class FigureXY2(Figure):
         else:
             self.plot_data(x,y)
 
-        self._draw()
+        self.canvasdraw()
     #clears the whole graph
     def clear_graph(self):
         while (len(self._axy.lines)):
@@ -110,7 +118,7 @@ class FigureXY2(Figure):
             while (len(self._axy2.lines)):
                 self._axy2.lines[-1].remove()
         self._init_figure()
-        self.draw()
+        self.canvasdraw()
 
     #clears the last point
     def delete_last_point(self):
@@ -123,7 +131,7 @@ class FigureXY2(Figure):
             if len(self._axy2.lines)!=0:
                 y2=self._axy2.lines[-1].get_ydata()
                 self._axy2.lines[-1].set_ydata(y2[:-1])
-        self._draw()
+        self.canvasdraw()
 
     #updates labels
     def update_labels(self,*args,**kwargs):
@@ -143,5 +151,4 @@ class FigureXY2(Figure):
             if 'y2' in kwargs and self._y2:
                 self._update_y2_label(kwargs['y2'])
             return
-        self._draw()
-
+        self.canvasdraw()
